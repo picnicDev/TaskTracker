f58b93b04aeddad8baee4d61db6c6b7d4d84e138
Sangmin Kim <offiksm0903@naver.com>
2026-08-04 15:07:00 +0900

    Initial Commit

---
+++ TaskTracker/TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by picnic on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
